ab057d4357cfc34d294cd45a7c36469f0b4d4354
Jeffrey Chiu <mr.jeffrey.chiu@gmail.com>
2020-11-17 18:43:58 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
 # code here!
+class School
+    attr_accessor :name, :roster
+
+    def initialize (name, roster={})
+    @name = name
+    @roster = roster
+    end
+
+    def add_student(v, k)
+        if !roster[k]
+            roster[k] = []
+        end
+        roster[k].push(v)
+    end
+
+    def grade(num)
+        p roster[num]
+    end
+
+    def sort
+        @roster.each do |k, v|
+            v.sort!
+        end
+    end
+end
